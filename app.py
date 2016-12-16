3863355bafd1f76367a17a9ed7ee0eef76f625e0
Swatch <luncasub11@gmail.com>
2016-12-16 12:38:26 +0200

    update

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def send_message(recipient_id, message_text):
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     
-	return str(r.status_code)+r.text
+	return str(r.status_code)
 
 
 
